a2471684e779eca25a74aebc5b059d5531f1fd05
Nedas Cerneckis <nedascerneckis@M1-Pro-2.local>
2022-03-09 21:38:20 +0000

    cleaned up unused files

--- gaussian_noise.m
+++
@@ -1,5 +0,0 @@
-function img = gaussian_noise(sigma, input_img)
-  noise = randn(size(input_img)) .* sigma;
-  disp(noise);
-  img = input_img + uint8(noise);
-end
--- untitled.m
+++
@@ -1,3 +0,0 @@
-figure;
-subplot(1,2,1), imshow(rand_img);
-subplot(1,2,2), imshow(img);
